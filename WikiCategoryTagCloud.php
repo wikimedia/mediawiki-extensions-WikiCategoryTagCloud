fee1063d94267971858adc4d7faf2a163bdbb7cf
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-08-29 02:07:21 -0700

    Update extension metadata. - License blurb - Author lines - Other attributes - Path in credits
    Change-Id: Ia8e00bb851a9e08c5d9b2f963f1b9a967f613d4a

--- WikiCategoryTagCloud.php
+++ WikiCategoryTagCloud.php
@@ -1,26 +1,15 @@
 <?php
 /**
  * Wiki Category Tag Cloud
+ *
  * @file
  * @ingroup Extensions
- * @author Daniel Friesen http://daniel.friesen.name
+ * @author Daniel Friesen (http://danf.ca/mw/)
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link https://www.mediawiki.org/wiki/Extension:WikiCategoryTagCloud Documentation
  * @version 1.1
  *
  * Derived from: YetAnotherTagCloud http://orangedino.com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
@@ -28,9 +17,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 }
 
 $wgExtensionCredits['parserhook'][] = array(
+	'path' => __FILE__,
 	'name' => 'Wiki Category Tag Cloud',
 	'version' => '1.1',
-	'author' => '[http://mediawiki.org/wiki/User:Dantman Daniel Friesen]',
+	'author' => '[http://danf.ca/mw/ Daniel Friesen]',
 	'description' => 'A Category Tag Cloud derived, improved, and fixed from the YetAnotherTagCloud Extension',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:WikiCategoryTagCloud',
 );
