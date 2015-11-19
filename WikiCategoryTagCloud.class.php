3142e48296c06cabb9f4634d81cf0b51aca2e16e
Jack Phoenix <jack@countervandalism.net>
2015-11-19 20:32:50 +0200

    WikiCategoryTagCloud: version 1.3 -- lots of security fixes and some other enhancements
    * SECURITY: XSS vector with the following extension parameters (as in <tagcloud some_param="value">): style, class, linkstyle, linkclass
    * SECURITY: possible SQL injection point with the exclude parameter (<tagcloud>exclude=SomeCategory,Another</tagcloud) -- the code now uses Database's makeList() properly and should be secure.
    * SECURITY: made WikiCategoryTagCloud::getBoxExtensionOption() more robust by calling htmlspecialchars() with the ENT_QUOTES parameter, as recommended by [[mw:XSS]]
    * Added extension.json file
    * Moved extension's main logic inside a class into a new file (which makes this diff rather unreadable, sorry)
    * Added extension's CSS from the MW.org info page into a new, separate CSS file which is loaded via ResourceLoader for pages which have the <tagcloud> tag (see WikiCategoryTagCloud::renderTagCloud(), specifically line 72 of WikiCategoryTagCloud.class.php)
    * Bumped version number to 1.3 and added myself to authors
    
    ShoutWiki SVN r3727
    
    Bug: T119118
    Bug: T119119
    
    Change-Id: I88612c021983dacac623e11cc87af158ea0b636d

---
+++ WikiCategoryTagCloud.class.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Wiki Category Tag Cloud
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Daniel Friesen http://daniel.friesen.name
+ *
+ * Derived from: YetAnotherTagCloud http://orangedino.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+class WikiCategoryTagCloud {
+	/**
+	 * Register the new <tagcloud> tag with the Parser.
+	 */
+	public static function register( &$parser ) {
+		$parser->setHook( 'tagcloud', array( __CLASS__, 'renderTagCloud' ) );
+		return true;
+	}
+
+	/**
+	 * When an admin edits MediaWiki:Tagcloudpages, purge the cache for each page
+	 * listed on that message.
+	 */
+	public static function invalidateCache( &$article, &$user, &$text, &$summary, $minor, $watchThis, $sectionAnchor, &$flags, &$status ) {
+		$at = $article->getTitle();
+
+		if ( $at->getText() == 'Tagcloudpages' && $at->getNamespace() == NS_MEDIAWIKI ) {
+			$message = wfMessage( 'tagcloudpages' )->inContentLanguage();
+
+			// If it's empty, do nothing.
+			if ( $message->isDisabled() ) {
+				return true;
+			}
+
+			$titles = explode( "\n", $message->plain() );
+
+			for ( $i = 0; $i < count( $titles ); $i++ ) {
+				$t = Title::newFromText( $titles[$i] );
+				if ( $t ) {
+					$t->invalidateCache();
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Callback function for register() which renders the HTML that this tag
+	 * extension outputs.
+	 */
+	public static function renderTagCloud( $input, $params, $parser ) {
+		$MIN_SIZE = 77;
+		$INCREASE_FACTOR = 100;
+
+		// Add CSS into the output via ResourceLoader
+		$parser->getOutput()->addModuleStyles( 'ext.wikicategorytagcloud' );
+
+		$dbr = wfGetDB( DB_SLAVE );
+
+		$cloudStyle = ( isset( $params['style'] ) ? Sanitizer::checkCss( $params['style'] ) : '' );
+		$cloudClasses = preg_split( '/\s+/', ( isset( $params['class'] ) ? htmlspecialchars( $params['class'], ENT_QUOTES ) : '' ) );
+		array_unshift( $cloudClasses, 'tagcloud' );
+		$linkStyle = ( isset( $params['linkstyle'] ) ? Sanitizer::checkCss( $params['linkstyle'] ) : '' );
+		$linkClasses = preg_split( '/\s+/', ( isset( $params['linkclass'] ) ? htmlspecialchars( $params['linkclass'], ENT_QUOTES ) : '' ) );
+		$minCountInput = self::getBoxExtensionOption( $input, 'min_count' );
+		$minSizeInput = self::getBoxExtensionOption( $input, 'min_size' );
+		$increaseFactorInput = self::getBoxExtensionOption( $input, 'increase_factor' );
+		if ( $minSizeInput != null ) {
+			$MIN_SIZE = $minSizeInput;
+		}
+		if ( $increaseFactorInput != null ) {
+			$INCREASE_FACTOR = $increaseFactorInput;
+		}
+		if ( $minCountInput == null ) {
+			$minCountInput = 0;
+		}
+
+		$excludedInput = self::getBoxExtensionOption( $input, 'exclude' );
+
+		$excludeCondition = array();
+		// If there are categories to be excluded, explode the "exclude=" line
+		// along commas and create an appropriate NOT IN condition for the SQL
+		// query below, escaping everything properly.
+		if ( strlen( $excludedInput ) > 0 ) {
+			$excludedCategories = explode( ',', $excludedInput );
+			if ( count( $excludedCategories ) > 0 ) {
+				$excludeCondition2 = 'cl_to NOT IN (';
+				$excludeCondition2 = $excludeCondition2 . $dbr->makeList( $excludedCategories );
+				$excludeCondition = array( $excludeCondition2 . ')' );
+			}
+		}
+
+		$res = $dbr->select(
+			'categorylinks',
+			array( 'cl_to AS title', 'COUNT(*) AS count' ),
+			$excludeCondition,
+			__METHOD__,
+			array(
+				'GROUP BY' => 'cl_to',
+				'HAVING' => "COUNT(*) >= {$minCountInput}",
+				'ORDER BY' => 'cl_to ASC'
+			)
+		);
+		$count = $dbr->numRows( $res );
+
+		$htmlOut = '';
+		$htmlOut = $htmlOut . Html::openElement( 'div', array(
+			'class' => implode( ' ', $cloudClasses ),
+			'style' => $cloudStyle ) );
+
+		$min = 1000000;
+		$max = -1;
+
+		for ( $i = 0; $i < $count; $i++ ) {
+			$obj = $dbr->fetchObject( $res );
+			$tags[$i][0] = $obj->title;
+			$tags[$i][1] = $obj->count;
+			if ( $obj->count < $min ) {
+				$min = $obj->count;
+			}
+			if ( $obj->count > $max ) {
+				$max = $obj->count;
+			}
+		}
+
+		for ( $i = 0; $i < $count; $i++ ) {
+			$textSize = $MIN_SIZE + ( $INCREASE_FACTOR * ( $tags[$i][1] ) ) / ( $max );
+			$title = Title::makeTitle( NS_CATEGORY, $tags[$i][0] );
+			$style = $linkStyle;
+			if ( $style != '' && substr( $style, -1 ) != ';' ) {
+				$style .= ';';
+			}
+			$style .= "font-size: {$textSize}%;";
+			$currentRow = '<a class="' . implode( ' ', $linkClasses ) .
+				"\" style=\"{$style}\" href=\"" . $title->getLocalURL() . '">' .
+				$title->getText() . '</a>&#160; ';
+			$htmlOut = $htmlOut . $currentRow;
+		}
+		$htmlOut = $htmlOut . '</div>';
+		return $htmlOut;
+	}
+
+	public static function getBoxExtensionOption( $input, $name, $isNumber = false ) {
+		if ( preg_match( "/^\s*$name\s*=\s*(.*)/mi", $input, $matches ) ) {
+			if ( $isNumber ) {
+				return intval( $matches[1] );
+			} else {
+				return htmlspecialchars( $matches[1], ENT_QUOTES );
+			}
+		}
+	}
+
+}
